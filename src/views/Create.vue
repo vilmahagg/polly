4c71fad918be4b66a1f429688a60a03737a1a7f4
Victoria Zubenko <victoriazubenko@Victorias-MacBook-Air-2.local>
2021-12-09 11:55:56 +0100

    småfix

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -179,23 +179,31 @@
           <span class="pollCode"> {{ pollId }}</span></h4>
       </div>
 
+      <section class="waitandstartButton">
+      <div class="stButton">
+        <router-link v-bind:to="'/result/' + pollId">
+          <button class="startButton">Start poll now!</button>
+        </router-link>
+      </div>
+
+      <br>
       <div class="wtButton">
         <router-link to="/">
           <button v-on:click="waitUntilLater" class="waitButton">Wait until later</button>
         </router-link>
       </div>
-      <br>
-      <div class="stButton">
-        <router-link v-bind:to="'/result/' + pollId">
-          <button class="startButton">Start poll now!</button>
-        </router-link>
+      </section>
+
+
+
+
 
 
 
 
       </div>
     </div>
-  </div>
+
 </template>
 
 <script>
@@ -696,13 +704,14 @@ header {
 
 }
 .stButton{
-  margin:90px 100px;
-  float:right;
+  width: available;
+  height: available;
+  position: relative;
+  margin-left: 25%;
 
 }
 .wtButton{
-  margin:100px 300px;
-  float:right;
+
 
 }
 
@@ -738,6 +747,11 @@ header {
   border: none;
 
 }
+
+.waitandstartButton{
+  margin-top: 130px;
+}
+
 
 
 </style>
