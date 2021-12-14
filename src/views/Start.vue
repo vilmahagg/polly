2d43208d6aaa70daf305ba2b7679e9d981d1db93
vilmahagg <vilmahggedelnn@gmail.com>
2021-12-14 17:38:30 +0100

    mobilanpassat och städat lite

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,180 +1,189 @@
 <template>
   <div class="all">
-  <div id="nav">
-    <header>
-      <h1 id="header">EasyPoll</h1>
+    <div id="nav">
+      <header>
+        <h1 id="header">EasyPoll</h1>
 
-      <p class="welcomeMessage">
-        {{ uiLabels.welcome }} <br />
-        {{ uiLabels.choose }}
-      </p>
+        <p class="welcomeMessage">
+          {{ uiLabels.welcome }} <br />
+          {{ uiLabels.choose }}
+        </p>
 
-      <div class="lang">
-        <button class="languageButton" v-on:click="switchLanguage">
-          <img v-bind:src="this.flag" />
-          <div class="hoverLang">Change Language</div>
+        <div class="headerStuff">
+          <div class="lang">
+            <button class="languageButton" v-on:click="switchLanguage">
+              <img v-bind:src="this.flag" />
+              <div class="hoverLang">Change Language</div>
+            </button>
+          </div>
+          <button class="aboutUs" v-on:click="showInfo">
+            {{ uiLabels.aboutUs }}
+          </button>
+
+          <!--<button class="aboutUs">{{ uiLabels.aboutUs }}</button>-->
+          <!--<button class="service">{{ uiLabels.service }}</button>-->
+          <button class="service" v-on:click="showHelp">
+            {{ uiLabels.service }}
+          </button>
+        </div>
+      </header>
+
+      <div class="firstMenu" v-if="!wantInfo && !wantHelp && !ready && !start">
+        <div class="create">
+          <router-link v-bind:to="'/create/' + lang" tag="button">
+            <button class="createButton">
+              <h3>{{ uiLabels.createPoll }}</h3>
+              <h5>{{ uiLabels.existing }}</h5>
+            </button>
+          </router-link>
+        </div>
+
+        <div class="participate" v-if="!ready && !start">
+          <button class="readyButton" v-on:click="isReady">
+            <h3>{{ uiLabels.participatePoll }}</h3>
+          </button>
+        </div>
+
+        <div class="buttonDiv">
+          <button
+            class="startExistButton"
+            v-on:click="startPoll"
+            v-if="!start && !isClicked"
+          >
+            <h4>{{ uiLabels.startExistingPoll }}</h4>
+          </button>
+        </div>
+      </div>
+
+      <div class="start" v-if="start && !ready">
+        <button class="back" v-on:click="isReady">
+          {{ uiLabels.backButton }}
         </button>
-      </div>
-      <button class="aboutUs" v-on:click="showInfo">
-        {{ uiLabels.aboutUs }}
-      </button>
+        <h2>Host existing poll</h2>
+        <div>
+          <label id="pollName">
+            {{ uiLabels.pollName }}
+            <input type="text" v-model="pollId" />
+          </label>
+        </div>
 
-      <!--<button class="aboutUs">{{ uiLabels.aboutUs }}</button>-->
-      <!--<button class="service">{{ uiLabels.service }}</button>-->
-      <button class="service" v-on:click="showHelp">
-        {{ uiLabels.service }}
-      </button>
-    </header>
-
-    <div class="firstMenu" v-if="!wantInfo && !wantHelp">
-
-      <div class="create" v-if="!ready && !start">
-        <router-link v-bind:to="'/create/' + lang" tag="button">
-          <button class="createButton">
-            <h3>{{ uiLabels.createPoll }}</h3>
-            <h5>{{ uiLabels.existing }}</h5>
-          </button>
-        </router-link>
+        <div>
+          <router-link v-bind:to="'/result/' + pollId" tag="button">
+            <button class="participateButton" v-on:click="runQuestion">
+              <h3>START</h3>
+            </button>
+          </router-link>
+        </div>
       </div>
 
-      <div class="participate" v-if="!ready && !start">
-        <button class="readyButton" v-on:click="isReady">
-          <h3>{{ uiLabels.participatePoll }}</h3>
+      <div class="start" v-if="ready">
+        <button class="back" v-on:click="isReady">
+          {{ uiLabels.backButton }}
         </button>
-      </div>
-
-    <div class="buttonDiv">
-      <button class="startExistButton" v-on:click="startPoll" v-if="!start && !isClicked">
-        <h4> {{uiLabels.startExistingPoll}} </h4>
-      </button>
-    </div>
-    </div>
-
-    <div class="start" v-if="start && !ready">
-      <button class="back" v-on:click="isReady">
-        {{ uiLabels.backButton }}
-      </button>
-      <div>
-      <label id="pollName">
-        {{ uiLabels.pollName }}
-        <input type="text" v-model="pollId" />
-      </label>
-    </div>
-
-    <div>
-        <router-link v-bind:to="'/result/' + pollId" tag="button">
-          <button class="participateButton" v-on:click="runQuestion">
-          <h3>START</h3>
-          </button>
-        </router-link>
-      </div>
-    </div>
-
-    <div class="start" v-if="ready">
-      <button class="back" v-on:click="isReady">
-        {{ uiLabels.backButton }}
-      </button>
-      <div>
-        <label id="pollName">
-          {{ uiLabels.pollName }}
-          <input type="text" v-model="id" />
-        </label>
-      </div>
-
-      <div>
-        <router-link v-bind:to="'/poll/' + id" tag="button">
-          <button class="participateButton" v-on:click="isClicked = true">
-            <h3>Start</h3>
-          </button>
-        </router-link>
-      </div>
-    </div>
-
-    <div class="help" v-if="wantHelp">
-      <button class="back" v-on:click="wantHelp = false" style="float: left">
-        {{ uiLabels.backButton }}
-      </button>
-
-      <h3>Customer Information</h3>
-      <p>If you have any concerns regarding the website please call: 073-397 70 53</p>
-
-    </div>
-
-    <div class="info" v-if="wantInfo">
-      <button class="back" v-on:click="wantInfo = false" style="float: left">
-        {{ uiLabels.backButton }}
-      </button>
-
-      <h3>General info and bussiness idea</h3>
-
-      <p>
-        The best webapplication ever made, easy pollcreation 4 the win <br />
-        EasyPoll works best in a classroom so the result can be shown on a big
-        screen for the students
-      </p>
-      <h3>The creators</h3>
-      <div class="creatorInfo">
-        <div class="vilmaInfo">
-          <p>Vilma Hägg Edelönn</p>
-          <img
-            class="vilma"
-            src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/14600832_1364866180211034_1798950174594637318_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=QEsj39RxgkQAX9eqH_1&_nc_ht=scontent-arn2-1.xx&oh=17508b40364919d9ae6db547e369a9d1&oe=61D62D74"
-          />
-          <p>
-            Instagram:
-            <a href="https://www.instagram.com/vilmahggedelnn/?hl=sv"
-              >VilmasInstagram</a
-            >
-          </p>
-          <p>Mail: vilmahggedelnn@gmail.com</p>
+        <h2>Participate in poll</h2>
+        <div>
+          <label id="pollName">
+            {{ uiLabels.pollName }}
+            <input type="text" v-model="id" />
+          </label>
         </div>
 
-        <div class="viktoriaSInfo">
-          <p>Viktoria Svensson</p>
-          <img
-            class="viktoriaS"
-            src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/22310701_1197227793711557_2200564642596610941_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=bjgP2Grj6N0AX9S451u&_nc_ht=scontent-arn2-1.xx&oh=ec3a8246e4883f999f04a34a5b609593&oe=61D82352"
-          />
-          <p>
-            Instagram :<a
-              href="https://www.instagram.com/viktoria_svensson/?hl=sv"
-              >VickansInstagram</a
-            >
-          </p>
-          <p>Mail: vilmahggedelnn@gmail.com</p>
+        <div>
+          <router-link v-bind:to="'/poll/' + id" tag="button">
+            <button class="participateButton" v-on:click="isClicked = true">
+              <h3>Start</h3>
+            </button>
+          </router-link>
         </div>
+      </div>
 
-        <div class="viktoriaZInfo">
-          <p>Viktoria Zubenko</p>
-          <img
-            class="viktoriaZ"
-            src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.6435-9/62450140_10156688965284055_2495346217672769536_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=vrKwecNNKPsAX-B2SIm&_nc_ht=scontent-arn2-1.xx&oh=679f4db2cc0df53639e7c43be4f014f5&oe=61D79D55"
-          />
-          <p>
-            Instagram:<a
-              href="https://www.instagram.com/victoriiazubenko/?hl=sv"
-              >VickansInstagram</a
-            >
-          </p>
-          <p>Mail: vilmahggedelnn@gmail.com</p>
-        </div>
+      <div class="help" v-if="wantHelp">
+        <button class="back" v-on:click="wantHelp = false" style="float: left">
+          X
+        </button>
 
-        <div class="aliceInfo">
-          <p>Alice Gardell</p>
-          <img
-            class="alice"
-            src="https://avatars.githubusercontent.com/u/93650060?v=4"
-          />
-          <p>
-            Instagram:<a href="https://www.instagram.com/alicegardell/?hl=sv"
-              >AliceInstagram</a
-            >
-          </p>
-          <p>Mail: alice.gardell@icloud.com</p>
+        <h3>Customer Information</h3>
+        <p>
+          If you have any concerns regarding the website please call: 073-397 70
+          53
+        </p>
+      </div>
+
+      <div class="info" v-if="wantInfo">
+        <button class="back" v-on:click="wantInfo = false" style="float: left">
+          X
+        </button>
+
+        <h3>General info and bussiness idea</h3>
+
+        <p>
+          The best webapplication ever made, easy pollcreation 4 the win <br />
+          EasyPoll works best in a classroom so the result can be shown on a big
+          screen for the students
+        </p>
+        <h3>The creators</h3>
+        <div class="creatorInfo">
+          <div class="vilmaInfo">
+            <p>Vilma Hägg Edelönn</p>
+            <img
+              class="vilma"
+              src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/14600832_1364866180211034_1798950174594637318_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=QEsj39RxgkQAX9eqH_1&_nc_ht=scontent-arn2-1.xx&oh=17508b40364919d9ae6db547e369a9d1&oe=61D62D74"
+            />
+            <p>
+              Instagram:
+              <a href="https://www.instagram.com/vilmahggedelnn/?hl=sv"
+                >VilmasInstagram</a
+              >
+            </p>
+            <p>Mail: vilmahggedelnn@gmail.com</p>
+          </div>
+
+          <div class="viktoriaSInfo">
+            <p>Viktoria Svensson</p>
+            <img
+              class="viktoriaS"
+              src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/22310701_1197227793711557_2200564642596610941_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=bjgP2Grj6N0AX9S451u&_nc_ht=scontent-arn2-1.xx&oh=ec3a8246e4883f999f04a34a5b609593&oe=61D82352"
+            />
+            <p>
+              Instagram :<a
+                href="https://www.instagram.com/viktoria_svensson/?hl=sv"
+                >VickansInstagram</a
+              >
+            </p>
+            <p>Mail: vilmahggedelnn@gmail.com</p>
+          </div>
+
+          <div class="viktoriaZInfo">
+            <p>Viktoria Zubenko</p>
+            <img
+              class="viktoriaZ"
+              src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.6435-9/62450140_10156688965284055_2495346217672769536_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=vrKwecNNKPsAX-B2SIm&_nc_ht=scontent-arn2-1.xx&oh=679f4db2cc0df53639e7c43be4f014f5&oe=61D79D55"
+            />
+            <p>
+              Instagram:<a
+                href="https://www.instagram.com/victoriiazubenko/?hl=sv"
+                >VickansInstagram</a
+              >
+            </p>
+            <p>Mail: vilmahggedelnn@gmail.com</p>
+          </div>
+
+          <div class="aliceInfo">
+            <p>Alice Gardell</p>
+            <img
+              class="alice"
+              src="https://avatars.githubusercontent.com/u/93650060?v=4"
+            />
+            <p>
+              Instagram:<a href="https://www.instagram.com/alicegardell/?hl=sv"
+                >AliceInstagram</a
+              >
+            </p>
+            <p>Mail: alice.gardell@icloud.com</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -193,7 +202,7 @@ export default {
       ready: false,
       wantInfo: false,
       wantHelp: false,
-      start:false,
+      start: false,
       isClicked: false,
       pollId: "",
       questionNumber: 0,
@@ -217,31 +226,30 @@ export default {
       }
       socket.emit("switchLanguage", this.lang);
     },
-    startPoll: function(){
-      this.start=true;
-      this.isClicked=true;
+    startPoll: function () {
+      this.start = true;
+      this.isClicked = true;
     },
-    isReady: function() {
-      if (this.ready==false && this.isClicked == false){
-   this.ready=true;
-   this.isClicked=true;
- }
- else{
-   this.ready=false;
-   this.isClicked=false;
- }
- this.start=false;
- },
+    isReady: function () {
+      if (this.ready == false && this.isClicked == false) {
+        this.ready = true;
+        this.isClicked = true;
+      } else {
+        this.ready = false;
+        this.isClicked = false;
+      }
+      this.start = false;
+    },
 
- showInfo: function(){
-   this.wantInfo = true;
-   this.wantHelp = false;
- },
- showHelp: function(){
-   this.wantInfo = false;
-   this.wantHelp = true;
- }
-/* runQuestion: function () {
+    showInfo: function () {
+      this.wantInfo = true;
+      this.wantHelp = false;
+    },
+    showHelp: function () {
+      this.wantInfo = false;
+      this.wantHelp = true;
+    },
+    /* runQuestion: function () {
    socket.emit("runQuestion", {
      pollId: this.pollId,
      questionNumber: this.questionNumber,
@@ -256,7 +264,7 @@ header {
   background-image: url("https://data.whicdn.com/images/334251300/original.gif");
   height: 12em;
   margin: 0;
-  margin-bottom:0.5em;
+  margin-bottom: 0.5em;
   /* border-radius: 25px; */
 }
 
@@ -279,11 +287,7 @@ header h1 {
 
 #nav {
   background: rgb(243, 220, 243);
-  height: 50em;
-}
-
-.firstMenu {
-  margin-top: 5em;
+  height: 100vh;
 }
 
 .aboutUs {
@@ -348,7 +352,6 @@ header h1 {
 }
 .service {
   float: left;
-
   background: none;
   border: none;
   text-decoration: underline;
@@ -362,6 +365,7 @@ h3 {
   display: grid;
   grid-gap: 0.3em;
   grid-template-columns: 50% 50%;
+  margin-top: 5em;
 }
 .firstMenu div {
   padding: 1em;
@@ -380,34 +384,15 @@ h3 {
   height: 10em;
 }
 
-.createButton {
-  width: 70%;
-  height: 100%;
-  background-size: 300% 100%;
-  border-radius: 25px;
-  text-transform: uppercase;
-
-  background-image: linear-gradient(
-    to right,
-    #e89aeb,
-    #b67ee4,
-    #b9a0e9,
-    #dc3cf1
-  );
-  box-shadow: 7px 7px 15px 0 rgba(199, 23, 190, 0.75);
-}
-
-.createButton:hover {
-  background-position: 100% 0;
-}
-
-.startExistButton{
-  width: 50%;
+.firstMenu button {
+  width: 90%;
   height: 100%;
   background-size: 300% 100%;
   border-radius: 25px;
   text-transform: uppercase;
   grid-template-columns: 50% 50%;
+  border: none;
+  box-shadow: inset 0 -0.9em 0 -0.35em rgba(0, 0, 0, 0.17);
 
   background-image: linear-gradient(
     to right,
@@ -416,10 +401,9 @@ h3 {
     #b9a0e9,
     #dc3cf1
   );
-  box-shadow: 7px 7px 15px 0 rgba(199, 23, 190, 0.75);
+  /* box-shadow: 7px 7px 15px 0 rgba(199, 23, 190, 0.75); */
 }
-
-.startExistButton:hover{
+.firstMenu button:hover {
   background-position: 100% 0;
 }
 
@@ -427,32 +411,45 @@ h3 {
   height: 10em;
 }
 
+/* PollId page to start or take poll */
+.start button {
+  padding: 0.35em 0.7em;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.15em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  font-weight: 400;
+  color: white;
+  box-shadow: inset 0 -0.6em 0 -0.35em rgba(0, 0, 0, 0.17);
+  text-align: center;
+  position: relative;
+  border: none;
+}
+.back {
+  float: left;
+  width: 5em;
+  height: 2em;
+  background-color: #862877;
+}
+
 .participateButton {
   width: 80%;
   height: 50%;
   background-color: rgb(164, 209, 164);
-  border-radius: 25px;
 }
 
-.readyButton {
-  width: 70%;
-  height: 100%;
-  background-color: rgb(161, 209, 161);
-  border-radius: 25px;
-  text-transform: uppercase;
-  background-size: 300% 100%;
-
-  background-image: linear-gradient(
-    to right,
-    #e89aeb,
-    #b67ee4,
-    #b9a0e9,
-    #dc3cf1
-  );
-  box-shadow: 7px 7px 15px 0 rgba(199, 23, 190, 0.75);
+.start input {
+  transition: 0.3s;
 }
-.readyButton:hover {
-  background-position: 100% 0;
+.start input:hover {
+  background-color: rgb(214, 214, 214);
+}
+.headerStuff{
+  margin:1em;
+  width:100%;
+
 }
 .lang {
   margin: 0;
@@ -465,12 +462,10 @@ h3 {
   font-weight: bold;
   width: 4em;
   height: 2em;
-  border:none;
+  border: none;
   background-color: rgba(0, 0, 0, 0);
 }
 .languageButton img {
-
-
   width: 3.3em;
   height: 1.6em;
 }
@@ -502,7 +497,6 @@ button:hover {
   padding: 1em;
 }
 
-
 #pollName {
   font-size: 1.5em;
 }
@@ -512,15 +506,27 @@ button:hover {
   width: 12em;
 }
 
-
 @media only screen and (max-width: 500px) {
   .all {
     font-size: 12px;
   }
 
-  h3{
-    font-size:1em;
+  h3 {
+    font-size: 1em;
   }
 
+  #nav{
+    padding:0;
+  }
+
+  .firstMenu button {
+    width: 100%;
+    height: 100%;
+  }
+  .headerStuff button{
+    margin:0;
+    padding-right:1em;
+    font-size:10px;
+  }
 }
 </style>
